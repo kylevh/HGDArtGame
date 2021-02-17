0b9623714da67df603e5e49c9009e0c913b7a6a8
kylevh <kylehmizu@gmail.com>
2021-02-17 12:15:33 -0800

    initial upload

---
+++ HGD Art Test/Assets/Scripts/Billboard.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera cam;
+    public bool useStaticBillboard;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (!useStaticBillboard)
+        {
+            transform.LookAt(cam.transform);
+        }
+        else
+        {
+            transform.rotation = cam.transform.rotation;
+        }
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    }
+}
