fe5ff0ccd2cfd4d98c7263888003ce9c17266dec
Vincent Allen <vallen813@gmail.com>
2021-03-06 04:27:08 -0800

    NPC Distance
    Added code for grabbing distances from NPC's and activating on a certain range.
    Still needs to be finished.
    See TODO in code for bugs and next steps

---
+++ HGD Art Test/Assets/Scripts/DistToNPC.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DistToNPC : MonoBehaviour
+{
+    public GameObject player;
+
+    public NPC[] npcList;
+
+    [Tooltip("The max distance you want the NPC to activate from")]
+    public float activationRange = 5;
+
+    Color gizmoColor = Color.green;
+    Vector3 pPos;
+    Vector3 nPos;
+
+    private void Awake()
+    {
+         npcList = FindObjectsOfType<NPC>();
+        pPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Loops through each NPC for distance calculations
+        foreach(NPC i in npcList)
+        {
+            // grabs the current npc's position, used purely for the gizmo function, alternative would be welcome
+            //TODO: Look more into gizmos so I don't need this hacky solution
+            nPos = i.pos;
+            // Distance function for two points on a 3D plane
+            float dist = Mathf.Sqrt(((pPos.x - i.pos.x) * (pPos.x - i.pos.x)) + ((pPos.y - i.pos.y) * (pPos.y - i.pos.y)) + ((pPos.z - i.pos.z) * (pPos.z - i.pos.z)));
+            // Checks if the distance is less than or equal to the activation range
+            if (dist <= activationRange)
+            {
+                // Set the gizmo color to red to indicate to dev that we're within activated range
+                gizmoColor = Color.red;
+                Debug.Log(i.ToString() + "   Distance to player: " + dist);
+            }
+            // Otherwise, reset gizmo to green to show we're not within activated range
+            else gizmoColor = Color.green;
+        }
+    }
+
+    //TODO: Resolve bug where gizmo only draws to the last npc in the array.
+    // Finding a way to draw multiple lines could be the solution
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        // Draws line between the player position and the npc's position
+        Gizmos.DrawLine(pPos, nPos);
+    }
+}
---
+++ HGD Art Test/Assets/Scripts/NPC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// NPC Object, allows the DistToNPC and any other classes to discover this NPC object. Additionally, stores the npc gameobject and position for easy access.
+/// </summary>
+public class NPC : MonoBehaviour
+{
+    public GameObject npc;
+    public Vector3 pos;
+
+    //TODO: Currently causes issues with gizmos (and other functions) when the NPC itself is moved. Could benefit from an update function, but I'm curious about a more performance alternative.
+    private void Awake()
+    {
+        npc = gameObject;
+        pos = transform.position;
+    }
+
+    /// <summary>
+    /// Overriden ToString to grab the objects name and position.
+    /// </summary>
+    /// <returns>Name and position</returns>
+    public override string ToString() { 
+        return "Name: " + npc.name + "\nPosition: " + pos; 
+    }
+}
