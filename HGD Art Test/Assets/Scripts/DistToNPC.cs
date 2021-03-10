86e0a3f4e2468adaa841b06ea44570167751f5a4
Vincent Allen <vallen813@gmail.com>
2021-03-09 18:05:50 -0800

    Completed DistToNPC
    The complete DistToNPC and NPC scripts. To see what status the NPC is in (Not near [0], near [1], and prompting the player[2]) use the ".getStatus()" method

--- HGD Art Test/Assets/Scripts/DistToNPC.cs
+++ HGD Art Test/Assets/Scripts/DistToNPC.cs
@@ -3,58 +3,132 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/// <summary>
+/// <list type="table">
+///     <item>Author:
+///         <description>Vincent Allen</description>
+///     </item>
+///     <item>
+///         <description>Calculates distance from player to all NPC's</description>
+///     </item>
+///     <item>
+///         <description>Sets NPC status based on that distance</description>
+///     </item>
+/// </list>
+/// <see cref="NPC"/> Hooks into NPC objects in the scene
+/// </summary>
 public class DistToNPC : MonoBehaviour
 {
     public GameObject player;
 
     public NPC[] npcList;
-    public ArrayList active = new ArrayList();
 
     [Tooltip("The max distance you want the NPC to activate from")]
     public float activationRange = 5;
 
-    Color gizmoColor = Color.green;
-    Vector3 pPos;
+    // Stores the position of the player, helps with reducing line-length
+    private Vector3 pPos;
 
     private void Awake()
     {
-         npcList = FindObjectsOfType<NPC>();
+        npcList = FindObjectsOfType<NPC>();
+        pPos = player.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Only checks distance if the player has moved
+        if (pPos != player.transform.position)
+        {
+            CalculateDistance();
+        }
         pPos = player.transform.position;
+        GizmoDraw();
+    }
+
+    /// <summary>
+    /// Calculates the distance between the player and NPC's
+    /// </summary>
+    public void CalculateDistance()
+    {
         // Loops through each NPC for distance calculations
         foreach (NPC i in npcList)
         {
-            bool closest = true;
-            // Distance function for two points on a 3D plane
-            float dist = Mathf.Sqrt(((pPos.x - i.pos.x) * (pPos.x - i.pos.x)) + ((pPos.y - i.pos.y) * (pPos.y - i.pos.y)) + ((pPos.z - i.pos.z) * (pPos.z - i.pos.z)));
-            i.setDist(dist);
-            // Checks if the distance is less than or equal to the activation range
-            if (dist <= activationRange)
-            {
-                Debug.Log(i.ToString() + "   Distance to player: " + dist);
-                foreach(NPC j in active)
-                {
 
-                    if (dist > j.distToPlayer) closest = false;
-                }
-                if (closest) { gizmoColor = Color.blue; }
-                // Set the gizmo color to red to indicate to dev that we're within activated range
-                else { gizmoColor = Color.red; }
-                if (!active.Contains(i))
-                {
-                    active.Add(i);
-                }
+            // Distance function for two points on a 3D plane
+            double dist = Mathf.Sqrt(((pPos.x - i.pos.x) * (pPos.x - i.pos.x)) + ((pPos.y - i.pos.y) * (pPos.y - i.pos.y)) + ((pPos.z - i.pos.z) * (pPos.z - i.pos.z)));
+            // Stores i's distance to the player onto the NPC itself
+            i.setDist(dist);
+
+            // Checks if the player is within the activationRange of the NPC
+            if (i.getDist() <= activationRange)
+            {
+                // The player is within i's activation range, so set its status to 1 and check to make sure it's not contesting with any other NPC's
+                i.setStatus(1);
+                ContestCheck(i);
             }
-            // Otherwise, reset gizmo to green to show we're not within activated range
+            // Otherwise, the player is not within this NPC's activated range
             else
             {
-                gizmoColor = Color.green;
-                if (active.Contains(i)) { active.Remove(i); }
+                i.setStatus(0);
             }
+        }
+    }
+
+    /// <summary>
+    /// <list type="table">
+    ///     <item>
+    ///         <description>Checks to see if NPC n is the only NPC that has the player within its activation range</description>
+    ///     </item>
+    ///     <item>
+    ///         <description>If there are other activated NPC's, then determine which NPC is closer and set their status accordingly</description>
+    ///     </item>
+    /// </list>
+    /// </summary>
+    /// <param name="n">NPC who's status we're determining</param>
+    public void ContestCheck(NPC n)
+    {
+        // NPC variable to store which NPC is closest, starts with n
+        NPC closestNPC = n;
+
+        foreach (NPC i in npcList)
+        {
+            //Debug.Log(closestNPC.ToString() + "   Distance to player: " + closestNPC.getDist());
+            // If i is closer to the player than the current closest NPC, and the closest NPC is at least within activation range, then make i the new closest NPC
+            if (i.getDist() <= closestNPC.getDist() && closestNPC.getStatus() >= 1)
+            {
+                // Sets the current closestNPC's status back to 1 in the event it was previously 2
+                closestNPC.setStatus(1);
+                // Sets closestNPC to i
+                closestNPC = i;
+                // Sets the new closestNPC's status to 2
+                closestNPC.setStatus(2);
+            }
+            // If i's status is already 2 and it is not the same as the closestNPC, set i's status back to 1
+            if(i.getStatus() == 2 && !i.Equals(closestNPC))
+            {
+                i.setStatus(1);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Draws gizmo lines in the scene editor to indicate what status the npc is in relative to the player distance
+    /// </summary>
+    public void GizmoDraw()
+    {
+        foreach (NPC i in npcList)
+        {
+            // Stores a color for easier gizmo color changing
+            Color gizmoColor = Color.green;
+
+            // Blue indicated the NPC is within activation range, but not prompting the player
+            if (i.getStatus() == 1) { gizmoColor = Color.blue; }
+            // Green means the npc is prompting the player to talk with them
+            else if (i.getStatus() == 2) { gizmoColor = Color.green; }
+            // Red means the npc is not within activation range
+            else { gizmoColor = Color.red; }
+
             Debug.DrawLine(pPos, i.pos, gizmoColor);
         }
     }
--- HGD Art Test/Assets/Scripts/NPC.cs
+++ HGD Art Test/Assets/Scripts/NPC.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// NPC Object, allows the DistToNPC and any other classes to discover this NPC object. Additionally, stores the npc gameobject and position for easy access.
+/// <list type="table">
+///     <item>Author:
+///         <description>Vincent Allen</description>
+///     </item>
+///     <item>
+///         <description>NPC Object, allows the DistToNPC and any other classes to discover this NPC object.</description>
+///     </item>
+///     <item>
+///         <description>Additionally, stores the npc gameobject and position for easy access.</description>
+///     </item>
+/// </list>
 /// </summary>
 public class NPC : MonoBehaviour
 {
     public GameObject npc;
+    // The current position of the NPC
     public Vector3 pos;
-    public float distToPlayer;
+
+    // Stores the current distance to the player
+    private double distToPlayer;
+    public int status = 0;
 
     //TODO: Currently causes issues with gizmos (and other functions) when the NPC itself is moved. Could benefit from an update function, but I'm curious about a more performance alternative.
     private void Awake()
@@ -18,9 +32,40 @@ public class NPC : MonoBehaviour
         pos = transform.position;
     }
 
-    public void setDist(float d)
+    /// <summary>
+    /// Returns the status of the NPC
+    /// </summary>
+    /// <returns>0 if not near, 1 if near, and 2 if prompting the player</returns>
+    public int getStatus()
     {
-        distToPlayer = d;
+        return this.status;
+    }
+
+    /// <summary>
+    /// Set status of NPC.   
+    /// 0 for not near, 1 for near, and 2 for prompting
+    /// </summary>
+    /// <param name="s">0 for not near, 1 for near, and 2 for prompting</param>
+    public void setStatus(int s)
+    {
+        status = s;
+    }
+
+    /// <summary>
+    /// Returns the distance to the player
+    /// </summary>
+    /// <returns>Distance to player</returns>
+    public double getDist()
+    {
+        return this.distToPlayer;
+    }
+    /// <summary>
+    /// Sets the distance to the player
+    /// </summary>
+    /// <param name="d">Distance to player</param>
+    public void setDist(double d)
+    {
+        this.distToPlayer = d;
     }
 
     /// <summary>
@@ -28,6 +73,20 @@ public class NPC : MonoBehaviour
     /// </summary>
     /// <returns>Name and position</returns>
     public override string ToString() { 
-        return "Name: " + npc.name + "\nPosition: " + pos; 
+        return "Name: " + npc.name + " Position: " + pos + " Status: " + status; 
+    }
+
+    /// <summary>
+    /// Overriden Equals to check if two NPC's are the same
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public override bool Equals(object other)
+    {
+        // If other is not of type NPC
+        if(!(other is NPC)) { return false; }
+        // If other does not have the same name as this NPC
+        if(((NPC)other).name != this.name) { return false; }
+        return true;
     }
 }
