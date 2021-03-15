ac07b108d7249e998dd56b1fc0d9c97965c18946
Hao Duong <howarddful123@gmail.com>
2021-03-14 17:23:37 -0700

    add temp texure and husky art for demo scence

--- HGD Art Test/Assets/Scripts/PlayerController.cs
+++ HGD Art Test/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// https://github.com/SebLague/Spherical-Gravity
+
 public class PlayerController : MonoBehaviour
 {
     // public vars
